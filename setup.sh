479b93edbd725190b1c1d2243a52d24b4cd5486a
Aakash Kaushik <kaushikaakash7539@gmail.com>
2020-11-12 21:32:57 +0530

    init setup.sh added

---
+++ setup.sh
@@ -0,0 +1,5 @@
+# Needs java for Apache spark.
+sudo apt-get install default-jre
+
+# Required Python packages.
+sudo pip3 install numpy pandas matplotlib tensorflow scikit-learn==0.22 pydotplus ipython gym scipy pyspark statsmodels
