73de3013fa3bc2b27822e56969c6ae12497ac3a6
Aakash Kaushik <kaushikaakash7539@gmail.com>
2020-11-13 02:20:49 +0530

    Done

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## Sundog Education
 Contents in the repository belong to [Sundog Education](https://sundog-education.com/) by Frank Kane
+
+Section-8: SparkPCA doesn't work.
--- setup.sh
+++ setup.sh
@@ -1,6 +1,19 @@
-# Needs java for Apache spark.
+# Java needed for Apache spark.
 sudo apt-get install default-jre
 
+# Needed for plotting of sklearn models
+sudo apt-get install graphviz
+
 # Required Python packages.
-sudo pip3 install numpy pandas matplotlib tensorflow scikit-learn==0.22 \
-	pydotplus ipython gym scipy pyspark statsmodels
+sudo pip3 install numpy==1.17.4 \
+				  pandas==1.1.2 \
+				  matplotlib==3.1.2 \
+				  tensorflow==2.3.1 \
+				  scikit-learn==0.22 \
+				  pydotplus==2.0.2 \
+				  ipython==7.19.0 \
+				  gym==0.17.3 \
+				  scipy==1.3.3 \
+				  pyspark==3.0.1 \
+				  statsmodels==0.12.1 \
+				  xgboost==1.2.1
