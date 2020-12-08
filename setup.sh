d874efb75583d2be7dd6804ad5599fdadbbee650
Aakash kaushik <kaushikaakash7539@gmail.com>
2020-12-08 19:45:45 +0530

    Closes #1

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ sudo apt-get install graphviz
 sudo pip3 install numpy==1.17.4 \
                   pandas==1.1.2 \
                   matplotlib==3.1.2 \
-                  tensorflow==2.3.1 \
+                  tensorflow-gpu==2.3.1 \
                   scikit-learn==0.22 \
                   pydotplus==2.0.2 \
                   ipython==7.19.0 \
