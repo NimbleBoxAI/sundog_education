514617737e3db2dbd646ac31ac8d87a29b42c73d
Aakash kaushik <kaushikaakash7539@gmail.com>
2020-12-31 10:59:32 +0530

    corrected tensorflow with cuda

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ sudo apt-get install graphviz
 sudo pip3 install numpy==1.17.4 \
                   pandas==1.1.2 \
                   matplotlib==3.1.2 \
-                  tensorflow-gpu==2.3.1 \
+                  tensorflow==2.2.0 \
                   scikit-learn==0.22 \
                   pydotplus==2.0.2 \
                   ipython==7.19.0 \
